532511f918e0a64d272a284177d06011b81091f2
David Shin <xdavidshinx@gmail.com>
2017-03-31 10:06:01 -0700

    add working functionality to styles.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
   $("#form").submit(function(event) {
     event.preventDefault();
+    $("#results").empty();
 
-    var input = $("#number").val()
+    var input = $("#number").val();
 
 //No negative numbers
     if(input<0){
       alert("Please pick a number that is greater than 0")
     }
 
-    for(var i=0;i<input;i++){
-
+    for(var i=1;i<=input;i++){
+      if(i%3===0 && i%5===0){
+        $('#results').append('<li>ping-pong</li>')
+      }else if(i%3===0){
+        $('#results').append('<li>ping</li>')
+      }else if(i%5===0){
+        $('#results').append('<li>pong</li>')
+      }else{
+        $('#results').append('<li>' + i + '</li>')
+        console.log(i);
+      }
     }
 });
 });
